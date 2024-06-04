82577dfef40a0d8e5a939ddffdb1ae35392fa8c3
Stephen Reidyck <stefaanriedijk@hotmail.com>
2024-06-04 12:49:55 +0200

    update wireui config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,14 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./vendor/wireui/wireui/src/*.php",
+        "./vendor/wireui/wireui/ts/**/*.ts",
+        "./vendor/wireui/wireui/src/WireUi/**/*.php",
+        "./vendor/wireui/wireui/src/Components/**/*.php",
     ],
-
+    presets: [
+        require("./vendor/wireui/wireui/tailwind.config.js"),
+    ],
     theme: {
         extend: {
             fontFamily: {
