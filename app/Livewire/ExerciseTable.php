fe0821b37ecc4565cbfe3805df71e6d899997e56
Stephen Reidyck <stefaanriedijk@hotmail.com>
2024-06-07 05:31:34 +0200

    updated search ux in table

--- app/Livewire/ExerciseTable.php
+++ app/Livewire/ExerciseTable.php
@@ -10,16 +10,38 @@ class ExerciseTable extends Component
 {
     use WithPagination;
     public $search = '';
+    public $target = '';
+    public $bodyPart = '';
+    public $equipment = '';
 
     public function updatingSearch()
     {
-        $this->reset();
+        $this->resetPage();
+    }
+    public function updateTarget() {
+        $this->render();
     }
     public function render()
     {
-        $exercises = WorkoutExercise::search($this->search)->paginate(20);
+//     $targetOptions = WorkoutExercise::query()->distinct()->pluck('body_part');
+//    dd($targetOptions);
+        $query = WorkoutExercise::query();
+
+        if (is_string($this->target) && $this->target !== '') {
+                $query->where('target', $this->target);
+            }
+
+        if (is_string($this->equipment) && $this->equipment !== '') {
+            $query->where('equipment', $this->equipment);
+        }
+
+        if (is_string($this->bodyPart) && $this->bodyPart !== '') {
+            $query->where('body_part', $this->bodyPart);
+        }
+
+        $exercises = $query->search($this->search)->paginate(20);
         return view('livewire.exercises.exercise-table',[
-            'exercises' => $exercises
+            'exercises' => $exercises,
         ]);
     }
 }
--- resources/views/livewire/exercises/exercise-table.blade.php
+++ resources/views/livewire/exercises/exercise-table.blade.php
@@ -1,12 +1,83 @@
 <div>
+    @php
+        $targetOptions = [
+        0 => "abs",
+        1 => "quads",
+        2 => "lats",
+        3 => "calves",
+        4 => "pectorals",
+        5 => "glutes",
+        6 => "hamstrings",
+        7 => "adductors",
+        8 => "triceps",
+        9 => "cardiovascular system",
+        10 => "spine",
+        11 => "upper back",
+        12 => "biceps",
+        13 => "delts",
+        14 => "forearms",
+        15 => "traps",
+        16 => "serratus anterior",
+        17 => "abductors",
+        18 => "levator scapulae"
+      ];
+    $equipmentOptions =
+    [0 => "body weight",
+    1 => "cable",
+    2 => "leverage machine",
+    3 => "assisted",
+    4 => "medicine ball",
+    5 => "stability ball",
+    6 => "band",
+    7 => "barbell",
+    8 => "rope",
+    9 => "dumbbell",
+    10 => "ez barbell",
+    11 => "sled machine",
+    12 => "upper body ergometer",
+    13 => "kettlebell",
+    14 => "olympic barbell",
+    15 => "weighted",
+    16 => "bosu ball",
+    17 => "resistance band",
+    18 => "roller",
+    19 => "skierg machine",
+    20 => "hammer",
+    21 => "smith machine",
+    22 => "wheel roller",
+    23 => "stationary bike",
+    24 => "tire",
+    25 => "trap bar",
+    26 => "elliptical machine",
+    27 => "stepmill machine"];
+    $bodyPartOptions = [
+    0 => "waist",
+    1 => "upper legs",
+    2 => "back",
+    3 => "lower legs",
+    4 => "chest",
+    5 => "upper arms",
+    6 => "cardio",
+    7 => "shoulders",
+    8 => "lower arms",
+    9 => "neck",
+  ]
+
+    @endphp
     <section class="mt-10">
         <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
             <div class="bg-white dark:bg-gray-800 overflow-hidden">
                 <div class="flex mb-6">
-                    <x-input wire:model.live="search" right-icon="search" label="Search:" placeholder="Search for exercises"
+                    <x-input wire:model.live="search" right-icon="search" label="Search:"
+                             placeholder="Search for exercises"
                              class="text-black w-60"/>
-                </div>
+                    <div class="ml-auto flex flex-row space-x-2">
 
+                        <x-select label="Target muscle" :options="$targetOptions" wire:model.live="target"/>
+                        <x-select label="Equipment" :options="$equipmentOptions" wire:model.live="equipment"/>
+                        <x-select label="Body part" :options="$bodyPartOptions" wire:model.live="bodyPart"/>
+                    </div>
+                </div>
                 <table class="border w-full">
                     <tr class="border-2">
                         <th class="capitalize bg-gray-600">Name</th>
@@ -15,6 +86,11 @@
                         <th class="capitalize bg-gray-600">Body Part</th>
                         <th class="capitalize bg-gray-600">Image</th>
                     </tr>
+                    @if($exercises->total()==0)
+                        <div><p>
+                                No results match your search.
+                            </p></div>
+                    @endif
                     @foreach ($exercises as $exercise)
 
                         <tr class="border border-black ml-4 py-1">
